9a7ee67530b7523dfb3babbad5a959fdcf26304b
Mr-Cornflake <zeplindavid@gmail.com>
2021-07-10 23:19:39 +0200

    bissl angepasst

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from "../components/CardList";
 import Scroll from "../components/Scroll";
 import Searchbox from "../components/SearchBox";
 import "./App.css";
+import ErrorBoundry from "../components/ErrorBoundry";
 
 class App extends Component {
   constructor() {
@@ -37,7 +38,9 @@ class App extends Component {
         <h1 className="f1">RoboFriends</h1>
         <Searchbox searchChange={this.onSearchChange} />
         <Scroll>
+          <ErrorBoundry>
           <CardList robots={filteredRobots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
